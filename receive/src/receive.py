b265f3459813498f58ecaa5b2c7187a1f8f1ba6e
Yves Wegner <Yves_Wegner@sluz.ch>
2023-05-22 14:26:39 +0200

    22.05.2023

---
+++ receive/src/receive.py
@@ -0,0 +1,44 @@
+import pymongo
+
+import os
+
+
+
+
+username = ""
+
+password = ""
+
+myclient = pymongo.MongoClient(f'mongodb+srv://{username}:{password}@memelab.swvil1y.mongodb.net/')
+
+mydb = myclient["Memelab"] #db name
+
+col_sub = mydb["subscribers"] # collection name
+
+
+
+class Database:
+
+        @staticmethod
+
+        def add_email_address(email_address:str): # Fügt eine Addresse hinzu
+
+                 try:
+
+                         email_address = email_address.lower()
+
+                        insert = {"address": email_address, "awards": {"first": 0, "second": 0, "third": 0}, "isSubscribed": True}
+
+                        if not email_address in Database.get_email_address(): # Wenn die Addresse nicht exestriert
+
+                        col_sub.insert_one(insert) # fügt hinzu
+
+                         else:
+
+                            print(f'Email already exists: {email_address})')
+
+                except Exception as error:
+
+                         print("Error: " + str(error))
+
+                         # TODO Log
