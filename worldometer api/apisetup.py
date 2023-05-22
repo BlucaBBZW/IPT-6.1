0e6ca98073a24ecd0433cde9f46692e791af4620
Marvin Zimmermann <marvin.zimmermann04@gmail.com>
2023-05-22 10:01:04 +0200

    apisetup model

--- worldometer api/apisetup.py
+++ worldometer api/apisetup.py
@@ -1,1 +1,34 @@
-import worldometer
+import worldometer;
+#Ranges
+def checki_deaththisyear () :
+    deathincrement = 0
+    tempstat = worldometer.deaths_this_year
+    deathincrement = worldometer.deaths_this_year - tempstat
+
+def checki_deathstoday () :
+    deathincrement = 0
+    tempstat = worldometer.deaths_today
+    deathincrement = worldometer.deaths_today - tempstat
+    
+def checki_deathbyalcohol () :
+    deathincrement = 0
+    tempstat = worldometer.deaths_caused_by_alcohol_this_year
+    deathincrement = worldometer.deaths_caused_by_alcohol_this_year - tempstat
+
+def checki_deathsbysmoking () :
+    deathincrement = 0
+    tempstat = worldometer.deaths_caused_by_smoking_this_year
+    deathincrement = worldometer.deaths_caused_by_smoking_this_year - tempstat
+    
+
+
+
+
+
+
+
+
+
+
+
+
